9c60f9dd47fd4d6ebd1ae08290a3efb49ad71037
Shawir Andhume <shawir.contact@gmail.com>
2026-04-17 21:49:19 +0100

    day 1: learn variables and basic types

---
+++ 01-javascript/variables.js
@@ -0,0 +1,11 @@
+// Practicing
+const email = "contact.test@esc.com"
+let statutCode = null 
+let button = false 
+const expectedTitle = "Checkout Page"
+
+// Affichage
+console.log("Email adress :", email)
+console.log("Statut Code :", statutCode)
+console.log("Affichage Bouton :", button)
+console.log("Title Page :", expectedTitle)
